60beb981261929a6c746d94c2016a3d01bf698a4
ifeanyihm <iheme.ifeanyi@yahoo.com>
2024-07-26 14:42:33 +0100

    task implement

--- app/_components/LinkListDesktop.tsx
+++ app/_components/LinkListDesktop.tsx
@@ -4,64 +4,99 @@ import Link from "next/link";
 import useLink from "../_context/useProduct";
 
 function LinkListDesktop() {
-  const { submittedData } = useLink();
+  const { submittedData, submittedProfile, imageFile } = useLink();
   return (
     <div className="relative hidden xl:w-[40.94%] bg-white xl:flex items-center justify-center rounded-[0.75rem]">
-      {submittedData.length > 0 ? (
-        <Image src="/assets/phonee.svg" alt="phone" width="307" height="631" />
-      ) : (
-        <Image src="/assets/phone.svg" alt="phone" width="307" height="631" />
-      )}
+      <div className="absolute top-[6.344rem]">
+        {submittedData.length > 0 ? (
+          <div>
+            <Image
+              src="/assets/phonee.svg"
+              alt="phone"
+              width="307"
+              height="631"
+            />
+          </div>
+        ) : (
+          <div>
+            <Image
+              src="/assets/phone.svg"
+              alt="phone"
+              width="307"
+              height="631"
+            />
+          </div>
+        )}
+        {submittedData.length > 0 ? (
+          <div className=" absolute top-[3.969rem] flex flex-col justify-center items-center  w-full  mx-auto text-center  ">
+            <div className="mb-[3.5rem]">
+              <div className="relative flex items-center justify-center w-[96px] h-[96px] mx-auto">
+                <Image
+                  src={imageFile}
+                  alt="profile picture"
+                  fill
+                  className="rounded-[50%]  border-[#633CFF] "
+                />
+              </div>
+              <h1 className="mt-[1.5rem] text-[1.3rem] font-bold text-[#333333]">
+                <span>{submittedProfile?.firstName || "Ben"}</span>{" "}
+                {submittedProfile?.lastName || "Wright"}
+              </h1>
+              <p className="text-[#737373]">
+                {submittedProfile?.email || "ben@example.com"}
+              </p>
+            </div>
 
-      {submittedData.length > 0 ? (
-        <ul className="dropdown-list absolute top-[23.75rem] list-none flex flex-col gap-[1.25rem] pl-[0.5rem] mb-[0.563rem] w-[15.813rem] h-[20rem] overflow-y-scroll">
-          {submittedData.map((data, index) => (
-            <li
-              key={index}
-              style={{
-                backgroundColor: data.color,
-              }}
-              className="rounded-lg border "
-            >
-              <Link
-                href={data.value}
-                target="_blank"
-                className="relative w-[full] flex items-center justify-between py-[0.58rem]"
-              >
-                <span className="absolute left-[1rem] bottom-[1rem]">
-                  <Image
-                    src={data.src}
-                    alt={data.name}
-                    width="16"
-                    height="18"
-                  />
-                </span>
-                <span
-                  className={`${
-                    data.color === "#fff" ? "text-[#333333]" : "text-white"
-                  }  pl-[2.75rem]`}
+            <ul className="dropdown-list list-none flex flex-col gap-[1.25rem] pl-[0.5rem] mb-[0.563rem] w-[15.813rem] h-[18.5rem] overflow-y-scroll">
+              {submittedData.map((data, index) => (
+                <li
+                  key={index}
+                  style={{
+                    backgroundColor: data.color,
+                  }}
+                  className="rounded-lg border "
                 >
-                  {data.name}{" "}
-                </span>
-                <span className=" pr-[1.313rem] ">
-                  <Image
-                    src={
-                      data.color !== "#fff"
-                        ? "/assets/arrow-right.svg"
-                        : "/assets/arr-right.svg"
-                    }
-                    alt="arrow right"
-                    width="16"
-                    height="16"
-                  />
-                </span>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <></>
-      )}
+                  <Link
+                    href={data.value}
+                    target="_blank"
+                    className="relative w-[full] flex items-center justify-between py-[0.813rem]"
+                  >
+                    <span className="absolute left-[1rem] bottom-[1.2rem]">
+                      <Image
+                        src={data.src}
+                        alt={data.name}
+                        width="16"
+                        height="18"
+                      />
+                    </span>
+                    <span
+                      className={`${
+                        data.color === "#fff" ? "text-[#333333]" : "text-white"
+                      }  pl-[2.75rem]`}
+                    >
+                      {data.name}{" "}
+                    </span>
+                    <span className=" pr-[1.313rem] ">
+                      <Image
+                        src={
+                          data.color !== "#fff"
+                            ? "/assets/arrow-right.svg"
+                            : "/assets/arr-right.svg"
+                        }
+                        alt="arrow right"
+                        width="16"
+                        height="16"
+                      />
+                    </span>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
     </div>
   );
 }
